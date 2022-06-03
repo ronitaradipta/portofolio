05a4ab90d4887f16929940c7174aaff707f59053
ronitaradipta <taradipta.roni@gmail.com>
2022-06-03 11:40:49 +0800

    push project pertama kali

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+let id = 0;
+class Portfolio extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/portfolio/" + projects.image;
+
+      return (
+        <div key={id++} className="columns portfolio-item">
+          <div className="item-wrap">
+            <div style={{ textAlign: "center" }}>{projects.title}</div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {projects}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
